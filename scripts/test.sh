796fcc1a6997787cfb3d6765ee9daaab9e682a2f
bdnf <oleg.bdnf@gmail.com>
2020-02-26 02:10:24 +0100

    added failing pylint example

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-exclude_linter_rules="--disable=C0103 \
-                        --disable=C0321 \
-                        --disable=W1201 \
-                        --disable=R1705 \
-                        --disable=E1101 \
-                        --disable=W0614 \
-                        --disable=W0401 \
-                        --disable=W0221 \
-                        --disable=R1705"
+exclude_linter_rules="--disable=R1705"
 
 docker run --rm --name=test-container -p 8000:80 "$@" pylint $exclude_linter_rules \
                                                       /app/app.py
